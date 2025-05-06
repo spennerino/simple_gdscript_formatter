1de6d09aa0592ec441425616b3d42e739a44bd25
IvonWei <madwind.cn@gmail.com>
2025-05-06 15:31:05 +0800

    Add a blank line at the end

--- addons/simple_gdscript_formatter/rules/blank_lines.gd
+++ addons/simple_gdscript_formatter/rules/blank_lines.gd
@@ -6,7 +6,8 @@ static func apply(code: String) -> String:
 
 	var trim_triple_newlines := RegEx.create_from_string("\n{3,}")
 	code = trim_triple_newlines.sub(code, "\n\n\n", true)
-
+	if code.length() > 0 and not code.ends_with("\n\n"):
+		code += "\n"
 	return code
 
 
@@ -16,7 +17,6 @@ static func _ensure_blank_lines_before_declarations(code: String) -> String:
 
 	var lines := code.split('\n')
 	var result_lines: Array[String] = []
-
 	for line: String in lines:
 		if declaration_regex.search(line):
 			if result_lines.size() > 0:
